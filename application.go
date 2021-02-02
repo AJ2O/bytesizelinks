db6a45d18ad8083e581aac1a8c87c1c19b4cd5cc
Juwon44 <juwonobat@gmail.com>
2021-02-01 21:57:20 -0500

    initial commit

---
+++ application.go
@@ -0,0 +1,110 @@
+package main
+
+import (
+	//"bytes"
+	//"encoding/json"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"text/template"
+
+	"github.com/gorilla/mux"
+)
+
+// HomePage defines values used for display on the home page
+type HomePage struct {
+	WebsiteURL       string
+	GeneratedURL     string
+	Message          string
+	GeneratedMessage bool
+	ErrorMessage     bool
+}
+
+const byteSizeLinksURL = "https://bsl.link"
+const apiURL = "https://z6wwpjwwqi.execute-api.us-east-1.amazonaws.com/dev"
+
+func generateLink(sourceLink string) (string, error) {
+	// TODO: validate source link
+	// 1. Empty?
+	// 2. Valid Link?
+
+	// Invoke API
+	response, err := http.Post(apiURL+"/?sourceLink="+sourceLink, "text/plain", nil)
+	if err != nil {
+		return "", err
+	}
+	data, _ := ioutil.ReadAll(response.Body)
+	stringData := string(data)
+	stringData = stringData[1 : len(stringData)-1]
+
+	return stringData, nil
+}
+func getLink(byteLink string) (string, error) {
+	// TODO: validate input link
+	// 1. Empty?
+
+	// Invoke API
+	response, err := http.Get(apiURL + "/?byteLink=" + byteLink)
+	if err != nil {
+		return "", err
+	}
+	data, _ := ioutil.ReadAll(response.Body)
+	stringData := string(data)
+	stringData = stringData[1 : len(stringData)-1]
+
+	return stringData, nil
+}
+
+func main() {
+	// router creation
+	r := mux.NewRouter()
+
+	// home
+	r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		tmpl, err := template.ParseFiles("home.html")
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		data := HomePage{
+			WebsiteURL:       byteSizeLinksURL,
+			GeneratedURL:     "",
+			Message:          "",
+			ErrorMessage:     false,
+			GeneratedMessage: false,
+		}
+
+		// post-request -> generate URL
+		if r.Method == http.MethodPost {
+			sourceLink := r.FormValue("sourceLink")
+			//requestLink := r.FormValue("requestLink")
+
+			link, err := generateLink(sourceLink)
+			if err != nil {
+				data.ErrorMessage = true
+			} else {
+				data.GeneratedMessage = true
+				data.Message = link
+			}
+		}
+
+		tmpl.Execute(w, data)
+	})
+
+	// re-routing to other pages
+	r.HandleFunc("/{byteLink}", func(w http.ResponseWriter, r *http.Request) {
+		vars := mux.Vars(r)
+		byteLink := vars["byteLink"]
+
+		link, err := getLink(byteLink)
+		if err != nil {
+			//
+		} else {
+			// re-route to website
+			log.Println(link)
+			http.Redirect(w, r, link, http.StatusSeeOther)
+		}
+	})
+
+	http.ListenAndServe(":80", r)
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/AJ2O/bytesizelinks
+
+go 1.15
+
+require github.com/gorilla/mux v1.8.0
